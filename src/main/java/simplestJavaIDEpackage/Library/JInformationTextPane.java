79ab57424e45ca86aeb62d8d9024734c95333c9d
Premaider <daniel.trageser@outlook.com>
2023-01-16 17:03:07 +0100

    added custom user information pane

---
+++ src/main/java/simplestJavaIDEpackage/Library/JInformationTextPane.java
@@ -0,0 +1,15 @@
+package simplestJavaIDEpackage.Library;
+
+import javax.swing.JTextPane;
+
+/**
+ * @author Daniel Trageser
+ */
+public class JInformationTextPane extends JTextPane {
+	private static final long serialVersionUID = 4716313567540658604L;
+
+	public void append(String text) {
+		String tmp = this.getText();
+		this.setText(tmp + "\n" + text);
+	}
+}
