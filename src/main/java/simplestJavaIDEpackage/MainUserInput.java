3dd480e47f68fca067cec0d5b9d6d0d74a36670b
daniel <daniel.trageser@ludwig-geissler-schule.de>
2024-06-29 14:44:07 +0200

    externalised MethodManager from MainUserInput

--- src/main/java/simplestJavaIDEpackage/MainUserInput.java
+++ src/main/java/simplestJavaIDEpackage/MainUserInput.java
@@ -21,6 +21,7 @@ import simplestJavaIDEpackage.Library.CodeStructure.CodingFile;
 import simplestJavaIDEpackage.Library.CodeStructure.FileManager;
 import simplestJavaIDEpackage.Library.CodeStructure.Methods;
 import simplestJavaIDEpackage.Library.CodingArea;
+import simplestJavaIDEpackage.Library.MethodManagerPanel;
 import simplestJavaIDEpackage.Library.TerminalPanel;
 
 /**
@@ -29,7 +30,6 @@ import simplestJavaIDEpackage.Library.TerminalPanel;
 public class MainUserInput {
 
   private final List<CodingArea> listOfCodingAreas = new ArrayList<>();
-  private final DefaultListModel<String> listModel = new DefaultListModel<>();
   private JFrame frmSimplestJavaIDE;
   private TerminalPanel terminal;
   private JTabbedPane tabbedPaneMethods;
@@ -163,38 +163,12 @@ public class MainUserInput {
       tabbedPaneMethods.insertTab(i.getMethod().getName(), null, i, null, index);
     }
 
-    // Panel for managing methods
-    JSplitPane splitPaneManagingMethods = new JSplitPane();
-    splitPaneManagingMethods.setOrientation(JSplitPane.VERTICAL_SPLIT);
-    JPanel inputPanel = new JPanel();
-    splitPaneManagingMethods.setLeftComponent(inputPanel);
-
-    JLabel labelImport = new JLabel("Method name:");
-    inputPanel.add(labelImport);
-
-    JTextField textFieldMethodName = new JTextField();
-    textFieldMethodName.setHorizontalAlignment(SwingConstants.CENTER);
-    textFieldMethodName.setText("");
-    inputPanel.add(textFieldMethodName);
-    textFieldMethodName.setColumns(25);
-
-    JList<String> listOfMethods = new JList<>(listModel);
-    for (Methods i : codingFile.methods) {
-      listModel.add(codingFile.methods.indexOf(i), i.getName());
-    }
-    splitPaneManagingMethods.setRightComponent(listOfMethods);
-
-    JButton btnAddMethod = getBtnAddMethod(codingFile, textFieldMethodName, mainCodingArea);
-    inputPanel.add(btnAddMethod);
-
-    JButton btnDeleteMethod = getBtnDeleteMethod(codingFile, listOfMethods);
-    inputPanel.add(btnDeleteMethod);
-
+    MethodManagerPanel methodManagerPanel = new MethodManagerPanel(codingFile, mainCodingArea, listOfCodingAreas, tabbedPaneMethods, terminal);
     JLabel labelManagingMethods = new JLabel("⚙");
     Font currentFont = labelManagingMethods.getFont();
     Font newFont = currentFont.deriveFont(currentFont.getSize() * 2F);
     labelManagingMethods.setFont(newFont);
-    tabbedPaneMethods.addTab("[Managing Methods]", splitPaneManagingMethods);
+    tabbedPaneMethods.addTab("[Managing Methods]", methodManagerPanel);
     // panel
     int indexOfManagingMethodsTab = -1;
     for (int i = 1; i < tabbedPaneMethods.getTabCount(); i++) {
@@ -317,53 +291,5 @@ public class MainUserInput {
               AddImportsWindow.launch(codingFile);
               FileManager.save(codingFile); // Also save other code
             });
-  }
-
-  private JButton getBtnDeleteMethod(CodingFile codingFile, JList<String> listOfMethods) {
-    JButton btnDeleteMethod = new JButton("Delete");
-    btnDeleteMethod.addActionListener(
-        arg0 -> {
-          int toDeleteIndex = listOfMethods.getSelectedIndex();
-          if (toDeleteIndex != 0) {
-            listModel.remove(toDeleteIndex); // remove listModel entry
-            codingFile.methods.remove(toDeleteIndex); // remove method
-            listOfCodingAreas.remove(toDeleteIndex); // remove CodingArea
-            tabbedPaneMethods.removeTabAt(toDeleteIndex); // remove tabbed pane
-          }
-        });
-    btnDeleteMethod.setPreferredSize(new Dimension(100, 36));
-    return btnDeleteMethod;
-  }
-
-  private JButton getBtnAddMethod(
-      CodingFile codingFile, JTextField textFieldMethodName, CodingArea mainCodingArea) {
-    JButton btnAddMethod = new JButton("Add");
-    btnAddMethod.addActionListener(
-        arg0 -> {
-          String newMethodName = textFieldMethodName.getText();
-          if (!newMethodName.isEmpty()) {
-            if (!newMethodName.contains(" ")) {
-              if ((!listModel.contains(newMethodName))
-                  && (!codingFile.checkIfMethodWithSameNameExists(newMethodName))) {
-                listModel.addElement(textFieldMethodName.getText());
-                codingFile.methods.add(
-                    new Methods(
-                        newMethodName, "public static void " + newMethodName + "(){\n\t\n}"));
-                int index = tabbedPaneMethods.getTabCount() - 1;
-                CodingArea newCodingArea =
-                    new CodingArea(
-                        codingFile.returnMethodFromName(newMethodName),
-                        terminal.getRunButton(),
-                        terminal.getSaveButton(),
-                        mainCodingArea.getTextArea().getFont());
-                listOfCodingAreas.add(newCodingArea);
-                tabbedPaneMethods.insertTab(newMethodName, null, newCodingArea, null, index);
-                FileManager.save(codingFile);
-              }
-            }
-          }
-        });
-    btnAddMethod.setPreferredSize(new Dimension(100, 36));
-    return btnAddMethod;
   }
 }
